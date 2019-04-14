6a833098a64b70b787f66b1c7113e1b608ef3c65
Emeka Augustine <cafafanscoders@gamil.com>
2019-04-14 00:48:46 -0400

    Added React Library with preset react command

---
+++ resources/js/core/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Example from './Example/Example';
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Example />
+            </div>
+        );
+    }
+}
+
+export default App;
